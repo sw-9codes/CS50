81490a7a7e96a2698356b8529d689df3cd75e8fb
Sierra Williams <124065193+sw-9codes@users.noreply.github.com>
2023-02-16 00:26:38 +0000

    Create debug.c

---
+++ debug.c
@@ -0,0 +1,15 @@
+// Become familiar wih C syntax
+// Learn to debug buggy code
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Ask for your name and where live
+    char *name = get_string("What is your name? ");
+    char *location = get_string("Where do you live? ");
+
+    // Say hello
+    printf("Hello, %s, from %s!\n", name, location);
+}
